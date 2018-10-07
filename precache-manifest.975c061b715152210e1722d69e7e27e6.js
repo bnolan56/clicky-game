13abc9f8da248e0ab1a2420083153f22fd0753a7
Brad Nolan <bnolan56@gmail.com>
2018-10-06 18:36:04 -0600

    Updates

---
+++ precache-manifest.975c061b715152210e1722d69e7e27e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "a90f083fb421d6903873",
+    "url": "/clicky-game/static/js/main.a90f083f.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clicky-game/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "a90f083fb421d6903873",
+    "url": "/clicky-game/static/css/main.c066e549.chunk.css"
+  },
+  {
+    "revision": "129eea4404ce3022dcdfb5eb3bcda362",
+    "url": "/clicky-game/index.html"
+  }
+];
